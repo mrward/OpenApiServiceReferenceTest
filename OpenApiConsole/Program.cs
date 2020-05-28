1e670aabd26063e30a8124c2c2bd8392f2a0233a
Matt Ward <matt.ward@microsoft.com>
2020-05-28 16:30:35 +0100

    Add console project
    Will call the web API via a generated Open API service reference
    after that is added.

---
+++ OpenApiConsole/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace OpenApiConsole
+{
+	class Program
+	{
+		static void Main (string[] args)
+		{
+			try {
+				Run ().Wait ();
+			} catch (Exception ex) {
+				Console.WriteLine (ex);
+			}
+		}
+
+		static async Task Run ()
+		{
+			var client = new swaggerClient ("https://localhost:5001/", new System.Net.Http.HttpClient ());
+			var forecasts = await client.WeatherForecastAsync ();
+			foreach (var forecast in forecasts) {
+				Console.WriteLine (forecast.Summary);
+			}
+		}
+	}
+}
